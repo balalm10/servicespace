1adc24b13c0f62bcd03dd8d156914b2d820a7a1b
balasiddharthk@gmail.com <balasiddharthk@gmail.com>
2021-03-15 02:35:35 +0530

    Added Profile Screen

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ const user = require('./models/user');
 const service = require('./models/service');
 const waterfall = require('async-waterfall');
 const path = require('path')
-const LocalStrategy = require("passport-local");
-const bodyParser  =   require("body-parser");
+const LocalStrategy = require('passport-local');
+const bodyParser = require('body-parser');
 const app = express();
-//Passport config
-//require('./config/passport')(passport);
 
-//swapped with DB config of MONGO ATLAS
 //DB config
-mongoose.connect('mongodb://127.0.0.1/servicespace', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
+mongoose.connect('mongodb://127.0.0.1/servicespace', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 //EJS
-//app.use(expressLayouts);
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 
 //BodyParser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended": true}));
+app.use(bodyParser.urlencoded({ 'extended': true }));
 
 
 //Express Session middleware
@@ -49,28 +45,28 @@ passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
 
-/*-------- Authentication Middleware --------*/
+/*------------------------ Authentication Middleware -----------------------*/
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()) {
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
-    req.flash("warning", 'Please log in first');
-    res.redirect("/signin");
+    req.flash('warning', 'Please log in first');
+    res.redirect('/signin');
 }
 
-function isNotLoggedIn(req, res, next){
-    if(!req.isAuthenticated()) {
+function isNotLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
         return next();
     }
-    req.flash("warning", 'You are Logged In');
-    res.redirect("/dashboard");
+    req.flash('warning', 'You are Logged In');
+    res.redirect('/profile');
 }
 
-/*-------------------------------------------*/
+/*-------------------------------------------------------------------------*/
 
 // GLOBAL VARS APP SCOPE
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.warning = req.flash('warning');
@@ -84,42 +80,41 @@ const UTYPE = {
     CUSTOMER: 'Customer'
 }
 
-
-app.get('/', (req, res) => {
-    console.log("Welcome")
-    res.render('home', {user: req.user, iLog: req.isAuthenticated()})
-});
-
-
 /*---------------------------------- Website Routes ----------------------------*/
 
-app.get('/signin', isNotLoggedIn,(req, res) =>{
-    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    res.render('signin', {user: req.user, iLog: req.isAuthenticated()})
-    //res.render('login', {default: "login"});
+app.get('/', (req, res) => {
+    console.log('Welcome')
+    res.render('home', { user: req.user, iLog: req.isAuthenticated() })
 });
 
-app.get('/dashboard', isLoggedIn, (req, res) => {
+app.get('/signin', isNotLoggedIn, (req, res) => {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    res.send(`<h1>Welcome ${req.user.name}</h1><hr><br>
-    <p>User Object retrieved from session : ${req.user}<br><br>
-    <a href="/logout">Logout</a></p>`)
+    res.render('signin', { user: req.user, iLog: req.isAuthenticated() })
+    //res.render('login', {default: 'login'});
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    res.render('profile', { user: req.user, iLog: req.isAuthenticated() })
+    // res.send(`<h1>Welcome ${req.user.name}</h1><hr><br>
+    // <p>User Object retrieved from session : ${req.user}<br><br>
+    // <a href='/logout'>Logout</a></p>`)
 })
 
-app.post("/login", isNotLoggedIn, passport.authenticate("local", {
-    successRedirect: "/dashboard",
+app.post('/login', isNotLoggedIn, passport.authenticate('local', {
+    successRedirect: '/profile',
     failureRedirect: '/signin',
     successFlash: true,
     failureFlash: true
-}),(req, res) => {
+}), (req, res) => {
     console.log(req.user)
-    req.flash("success", `Logged In. Welcome ${req.user.name}!`);
+    req.flash('success', `Logged In. Welcome ${req.user.name}!`);
 });
 
 //Logout handle
-app.get('/logout', isLoggedIn,(req, res) => {
+app.get('/logout', isLoggedIn, (req, res) => {
     req.logout();
-    req.flash('info','You are logged out');
+    req.flash('info', 'You are logged out');
     res.redirect('/signin');
 });
 
@@ -132,31 +127,31 @@ app.post('/signup', (req, res) => {
     console.log(req.body)
 
     user.register(new user({
-                    username: req.body.username,
-                    name: req.body.name,
-                    dob: req.body.dob,
-                    utype: req.body.utype,
-                    spdetails: (req.body.utype === UTYPE.CUSTOMER) ? {} : {
-                        services: [],
-                        phone: req.body.phone,
-                        email: req.body.email
-                    }
-                }),
-                req.body.password,
-                (err, user) => {
-                    if(err){
-                        console.log(err)
-                        req.flash("error", `Could not create account: ${err.message}`);
-                        res.redirect('/signin');
-                        return;
-                    }
-                    else{
-                        console.log('Created User', user)
-                        passport.authenticate("local")(req, res, function(){
-                            req.flash("success", `Account Created. Welcome ${req.user.name}!`);
-                            res.redirect("/dashboard");
-                        });
-                };
+        username: req.body.username,
+        name: req.body.name,
+        dob: req.body.dob,
+        utype: req.body.utype,
+        spdetails: (req.body.utype === UTYPE.CUSTOMER) ? {} : {
+            services: [],
+            phone: req.body.phone,
+            email: req.body.email
+        }
+    }),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                console.log(err)
+                req.flash('error', `Could not create account: ${err.message}`);
+                res.redirect('/signin');
+                return;
+            }
+            else {
+                console.log('Created User', user)
+                passport.authenticate('local')(req, res, function () {
+                    req.flash('success', `Account Created. Welcome ${req.user.name}!`);
+                    res.redirect('/profile');
+                });
+            };
         });
 });
 
@@ -165,7 +160,7 @@ app.post('/createservice', (req, res) => {
     console.log(req.body)
 
     // If stateless call (no session)
-    if(!req.user) {
+    if (!req.user) {
         req.user = req.body.user
     }
 
@@ -177,7 +172,7 @@ app.post('/createservice', (req, res) => {
             }
             else {
                 // err: true  (String evaluates to true)
-                cb('User is not a Service Provider')    
+                cb('User is not a Service Provider')
             }
         },
         function createNewService(cb) {
@@ -196,20 +191,20 @@ app.post('/createservice', (req, res) => {
         function addServiceToDB(service_obj, cb) {
             console.log('Saving service');
             service_obj.save((err, created_service) => {
-                if(err) {	
+                if (err) {
                     console.log(err);
                     cb('Error while creating service')
                 }
                 else {
-                    console.log("Created Service");
+                    console.log('Created Service');
                     cb(null, created_service)
                 }
             });
         },
         function linkServiceToServiceProvider(created_service, cb) {
             console.log('Linking Service to SP');
-            user.findOneAndUpdate({'_id':req.user._id}, { $addToSet: {"spdetails.services": created_service._id} }, function(err, user) {
-                if(err) {
+            user.findOneAndUpdate({ '_id': req.user._id }, { $addToSet: { 'spdetails.services': created_service._id } }, function (err, user) {
+                if (err) {
                     console.log(err);
                     cb('Could not add service')
                 }
@@ -218,25 +213,25 @@ app.post('/createservice', (req, res) => {
                     cb(null, created_service)
                 }
             });
-        }	
+        }
     ];
-        
-    waterfall(createService, function(err, created_service){
-        if(err) {
-            req.flash("error", err);
+
+    waterfall(createService, function (err, created_service) {
+        if (err) {
+            req.flash('error', err);
             console.log('Error in waterfall', err);
-            res.json({"error": true, "message": err})
+            res.json({ 'error': true, 'message': err })
             return;
         }
         req.flash('info', 'Added Service successfully');
         console.log('Success :', 'Added Service successfully');
-        res.json({"error": false, "message": created_service})
+        res.json({ 'error': false, 'message': created_service })
         return;
     });
 
 });
 
 
-const  PORT = 3000;
+const PORT = 3000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
